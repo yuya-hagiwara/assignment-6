980e5ed76b9f57cafe00025ccba8742490108a13
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-08-20 23:27:52 +0900

    camelCaseに修正。

---
+++ src/main/java/com/tutorial/hello/Human.java
@@ -0,0 +1,29 @@
+package com.tutorial.hello;
+
+public class Human {
+
+
+    private String name;
+    private int age;
+
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
+
+
+
+
+
